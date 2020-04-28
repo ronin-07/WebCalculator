be8bf883e7931b2ca2cdcbb0f19f5fcda6b9ced5
ronin-07exitbyegit config --global user.name ronin-07 <sidmudholkar07@gmail.com>
2020-04-28 14:37:33 +0530

    v1 recursive result

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 var numbers = [];
 var operators = [];
 var num = "";
-var countOp = 0;
+var text = "";
 
-var conv = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
-};
+// var conv = {'one': '1','two': '2','three': '3','four': '4','five': '5','six': '6','seven': '7','eight': '8','nine': '9'};
 
 $("button").click(function () {
-    if (this.classList[1] !== "op") {
-        num += conv[this.classList[1]];
+    text = text + this.classList[1];
+    $(".active").text(text);
+    if (this.classList[2] !== "op") {
+        num += this.classList[1];
     } else {
-        countOp++;
         numbers.push(Number(num));
-        operators.push(this.classList[2]);
+        operators.push(this.classList[1]);
         num = "";
     }
-    if (this.classList[2] === 'equal') {
+    if (this.classList[1] === 'equal') {
+        var result = results();
         $(".history").text(String(numbers[0]) + " " + operators[0] + " " + String(numbers[1]) + " =");
-        $(".active").text(results());
-        $("button").off();
+        $(".active").text("");
+        $(".active").text(result);
+        text = String(result);
+        num = String(result)
+        numbers.length = 0;
+        operators.length = 0;
     }
 })
 
